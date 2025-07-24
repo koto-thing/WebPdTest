e3424b00ae996ebc2ec5927b029585b720179dda
koto-thing <gotoukenta62@gmail.com>
2025-07-24 18:42:59 +0900

    デバイス変更

---
+++ src/Section/Home.tsx
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import "./Home.css";
+
+const Home: React.FC = () => {
+    return (
+        <div className="home-container">
+            <main>
+                <h1>Pure Data on Web</h1>
+                <p>このサイトでは、ブラウザ上で動作するPureDataパッチを体験できます。</p>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
